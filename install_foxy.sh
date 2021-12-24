31f2c864ec8ebcc74f822937ffe8330a56758b62
jamy <jamychahal@hotmail.com>
2021-12-24 14:39:07 +0100

    Add echo on install_foxy script

--- install_foxy.sh
+++ install_foxy.sh
@@ -5,3 +5,6 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
 sudo apt -y install ros-foxy-desktop
+
+#Echo only once
+echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
